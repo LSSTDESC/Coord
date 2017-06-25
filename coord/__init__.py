52dd00a1a5754fc9554bcd5252ea518ed234d245
Mike Jarvis <michael@jarvis.net>
2017-06-25 12:34:42 -0400

    Basics of setup.py, version, init

---
+++ coord/__init__.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2013-2017 LSST Dark Energy Science Collaboration (DESC)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# The version is stored in _version.py as recommended here:
+# http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
+from ._version import __version__, __version_info__
+import os,cffi,glob
+
+# Also let coord.version show the version.
+version = __version__
+
+# Set module level attributes for the include directory and the library file name.
+coord_dir = os.path.dirname(__file__)
+include_dir = os.path.join(coord_dir,'include')
+
+lib_file = os.path.join(coord_dir,'_coord.so')
+# Some installation (e.g. Travis with python 3.x) name this e.g. _coord.cpython-34m.so,
+# so if the normal name doesn't exist, look for something else.
+if not os.path.exists(lib_file): # pragma: no cover
+    alt_files = glob.glob(os.path.join(os.path.dirname(__file__),'_coord*.so'))
+    if len(alt_files) == 0:
+        raise IOError("No file '_coord.so' found in %s"%coord_dir)
+    if len(alt_files) > 1:
+        raise IOError("Multiple files '_coord*.so' found in %s: %s"%(coord_dir,alt_files))
+    lib_file = alt_files[0]
+
+# Load the C functions with cffi
+_ffi = cffi.FFI()
+for file_name in glob.glob(os.path.join(include_dir,'*_C.h')):
+    _ffi.cdef(open(file_name).read())
+_lib = _ffi.dlopen(lib_file)
+
+# Explicitly import things that we want to be in the coord namespace
+from .angleunit import AngleUnit, arcsec, arcmin, degrees, hours, radians
+from .angle import Angle, _Angle, DMS_Angle, HMS_Angle
+from .celestial import CelestialCoord
---
+++ setup.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+import sys,os,glob,re
+
+import setuptools
+from setuptools import setup, Extension
+
+print('Python version = ',sys.version)
+py_version = "%d.%d"%sys.version_info[0:2]  # we check things based on the major.minor version.
+
+dependencies = ['numpy', 'cffi']
+
+sources = glob.glob(os.path.join('src','*.cpp'))
+print('sources = ',sources)
+headers = glob.glob(os.path.join('include','*.h'))
+print('headers = ',headers)
+ext=Extension("coord._coord", sources, depends=headers, include_dirs=['include'])
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+# Read in the coord version from coord/_version.py
+# cf. http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
+version_file=os.path.join('coord','_version.py')
+verstrline = open(version_file, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    coord_version = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (version_file,))
+print('Coord version is %s'%(coord_version))
+
+dist = setup(name="LSSTDESC.Coord",
+      version=coord_version,
+      author="LSST DESC (contact: Mike Jarvis)",
+      author_email="michael@jarvis.net",
+      description="Python module for handling angles and celestial coordinates",
+      long_description=long_description,
+      license = "MIT License",
+      url="https://github.com/LSSTDESC/Coord",
+      download_url="https://github.com/LSSTDESC/Coord/releases/tag/v%s.zip"%coord_version,
+      packages=['coord'],
+      package_data={'coord' : headers },
+      ext_modules=[ext],
+      install_requires=dependencies)
