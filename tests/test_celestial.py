79ed699d3082a019cac732896d7018811aad14b8
Mike Jarvis <michael@jarvis.net>
2017-07-02 14:46:51 -0400

    Add test_lambert_projection

--- tests/test_celestial.py
+++ tests/test_celestial.py
@@ -93,7 +93,80 @@ def test_stereographic_projection():
 def test_lambert_projection():
     """Test the lambert projection.
     """
-    pass
+    # Test that a small triangle has the correct properties.
+    # Lambert projections preserve area, but not angles.
+    center = coord.CelestialCoord(0.234 * radians, 0.342 * radians)
+    cA = coord.CelestialCoord(-0.193 * radians, 0.882 * radians)
+    cB = coord.CelestialCoord((-0.193 + 1.7e-6) * radians,
+                               (0.882 + 1.2e-6) * radians)
+    cC = coord.CelestialCoord((-0.193 - 2.4e-6) * radians,
+                               (0.882 + 3.1e-6) * radians)
+
+    a = cB.distanceTo(cC).rad
+    b = cC.distanceTo(cA).rad
+    c = cA.distanceTo(cB).rad
+    A = cA.angleBetween(cB,cC).rad
+    B = cB.angleBetween(cC,cA).rad
+    C = cC.angleBetween(cA,cB).rad
+    E = cA.area(cB,cC)
+
+    uA, vA = center.project(cA, projection='lambert')
+    uB, vB = center.project(cB, projection='lambert')
+    uC, vC = center.project(cC, projection='lambert')
+    uA = uA / arcsec  # Easier to just deal with these in arcsec
+    vA = vA / arcsec
+    uB = uB / arcsec
+    vB = vB / arcsec
+    uC = uC / arcsec
+    vC = vC / arcsec
+
+    # The shoelace formula gives the area of a triangle given coordinates:
+    # A = 1/2 abs( (x2-x1)*(y3-y1) - (x3-x1)*(y2-y1) )
+    area = 0.5 * abs( (uB-uA) * (vC-vA) - (uC-uA) * (vB-vA) )
+    area *= (arcsec / radians)**2
+    print('lambert area = ',area,E)
+    np.testing.assert_almost_equal(area / E, 1, decimal=5)
+
+    # Check that project_rad does the same thing
+    uA2, vA2 = center.project_rad(cA.ra.rad, cA.dec.rad, projection='lambert')
+    np.testing.assert_array_almost_equal(uA, uA2, decimal=10)
+    np.testing.assert_array_almost_equal(vA, vA2, decimal=10)
+
+    # Check the deprojection
+    cA2 = center.deproject(uA*arcsec, vA*arcsec, projection='lambert')
+    np.testing.assert_almost_equal(cA.ra.rad, cA2.ra.rad, decimal=12)
+    np.testing.assert_almost_equal(cA.dec.rad, cA2.dec.rad, decimal=12)
+    cA3 = center.deproject_rad(uA, vA, projection='lambert')
+    np.testing.assert_array_almost_equal([cA.ra.rad, cA.dec.rad], cA3, decimal=12)
+
+    # The angles are not preserved
+    a = sqrt( (uB-uC)**2 + (vB-vC)**2 )
+    b = sqrt( (uC-uA)**2 + (vC-vA)**2 )
+    c = sqrt( (uA-uB)**2 + (vA-vB)**2 )
+    cosA = ((uB-uA)*(uC-uA) + (vB-vA)*(vC-vA)) / (b*c)
+    cosB = ((uC-uB)*(uA-uB) + (vC-vB)*(vA-vB)) / (c*a)
+    cosC = ((uA-uC)*(uB-uC) + (vA-vC)*(vB-vC)) / (a*b)
+
+    print('lambert cosA = ',cosA,cos(A))
+    print('lambert cosB = ',cosB,cos(B))
+    print('lambert cosC = ',cosC,cos(C))
+
+    # The deproject jacobian should tell us how the area changes
+    dudx, dudy, dvdx, dvdy = center.jac_deproject(uA*arcsec, vA*arcsec, 'lambert').ravel()
+    jac_area = abs(dudx*dvdy - dudy*dvdx)
+    np.testing.assert_almost_equal(jac_area, E/area, decimal=5)
+
+    dudx, dudy, dvdx, dvdy = center.jac_deproject_arcsec(uA, vA, 'lambert').ravel()
+    np.testing.assert_almost_equal(jac_area, abs(dudx*dvdy - dudy*dvdx))
+
+    # center projects to 0,0 with unit area
+    u, v = center.project(center, 'lambert')
+    np.testing.assert_almost_equal(u.rad, 0., decimal=12)
+    np.testing.assert_almost_equal(v.rad, 0., decimal=12)
+    c2 = center.deproject(u,v, 'lambert')
+    np.testing.assert_almost_equal(c2.ra.rad, center.ra.rad, decimal=12)
+    np.testing.assert_almost_equal(c2.dec.rad, center.dec.rad, decimal=12)
+    np.testing.assert_almost_equal(np.linalg.det(center.jac_deproject(u, v, 'lambert')), 1.)
 
 
 @timer
