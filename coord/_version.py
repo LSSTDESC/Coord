5d350024bc6e50418791c0cb1597c453e0b2c9db
Mike Jarvis <michael@jarvis.net>
2017-07-02 13:50:46 -0400

    Update version to 1.0.0

--- coord/_version.py
+++ coord/_version.py
@@ -18,5 +18,5 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__version__ = '1.0'
+__version__ = '1.0.0'
 __version_info__ = tuple(map(int, __version__.split('.')))
