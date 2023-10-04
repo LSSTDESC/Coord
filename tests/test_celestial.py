2b463cb324ea8e24027aae66d1072323cec215c4
beckermr <becker.mr@gmail.com>
2023-10-04 13:29:26 -0500

    TST use absolute tolerance to compare to zero

--- tests/test_celestial.py
+++ tests/test_celestial.py
@@ -818,7 +818,7 @@ def test_lambert_projection():
 
     # center projects to 0,0 with unit area
     u, v = center.project(center, 'lambert')
-    np.testing.assert_allclose([u.rad, v.rad], 0., err_msg='center did not project to (0,0)')
+    np.testing.assert_allclose([u.rad, v.rad], 0., err_msg='center did not project to (0,0)', atol=1e-16, rtol=0)
     c2 = center.deproject(u,v, 'lambert')
     np.testing.assert_allclose(c2.rad, center.rad, err_msg='(0,0) did not deproject to center')
     np.testing.assert_allclose(np.linalg.det(center.jac_deproject(u, v, 'lambert')), 1.,
